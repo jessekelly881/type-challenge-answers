42e05dfec498ddc7751c740f36d32c34be5b3973
Jesse Kelly <kellyjesse881@gmail.com>
2025-07-23 23:38:34 -0600

    easy/readonly

---
+++ src/easy/readonly.ts
@@ -0,0 +1,16 @@
+type MyReadonly<T> = {
+	readonly [k in keyof T]: T[k];
+};
+
+interface Todo {
+	title: string;
+	description: string;
+}
+
+const todo: MyReadonly<Todo> = {
+	title: "Hey",
+	description: "foobar",
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
