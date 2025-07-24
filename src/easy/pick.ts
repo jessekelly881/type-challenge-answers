9d5bdb0638199dd6cbf25653a61cd798d369ec1b
Jesse Kelly <kellyjesse881@gmail.com>
2025-07-23 23:33:34 -0600

    .

--- src/easy/pick.ts
+++ src/easy/pick.ts
@@ -1,3 +1,7 @@
+/**
+ * @see https://github.com/type-challenges/type-challenges/blob/main/questions/00004-easy-pick/README.md
+ */
+
 type MyPick<Obj, K extends keyof Obj> = { [k in K]: Obj[k] };
 
 interface Todo {
